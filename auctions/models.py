96aa2a674271d72b13d9696e25a16d2b57e22dfe
Andy L <oleskainbox@gmail.com>
2026-05-22 15:07:41 -0600

    Pushing updates to GitHub.

--- auctions/models.py
+++ auctions/models.py
@@ -24,3 +24,6 @@ class Listing(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True, related_name='category')
     active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} (listed by {self.seller} for {self.price})"
